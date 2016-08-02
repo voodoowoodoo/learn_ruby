246b0aab85a9b6364cae74b72782290757806565
Jorrit <jorrit_l@hotmail.com>
2016-08-02 23:05:20 +0200

    partial completion of pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,16 @@
 #write your code here
+def translate(s)
+	vowels = ['a','e','u','i','o']
+
+	if vowels.include?(s[0])
+		s = s + "ay"
+		return s
+		
+	else
+		firstLetter = s[0]
+		s[0] = ""
+		s = s + firstLetter + "ay"
+		return s
+		
+	end
+end
