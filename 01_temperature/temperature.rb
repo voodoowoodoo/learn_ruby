b46725802a4938f96bd7650bf62f190209f1d2b7
Jorrit <jorrit_l@hotmail.com>
2016-08-02 16:14:16 +0200

    Finished 01_Temperatures

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
 #write your code here
+
+#Working in floats because of fractional maths. 
+def ftoc(f)
+	c = (f - 32).to_f * (5.0 / 9.0)
+	return c
+end
+
+def ctof(c)
+	f = c.to_f * (9.0 / 5.0) + 32.0
+end
